d2596fa05fb9e33dfc5ded5747e0e8ac84d4c456
ohyujeong <ohlife4@gmail.com>
2023-01-09 16:40:01 +0900

    구간 합 구하기 4

---
+++ src/BOJ_11659.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_11659 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int j=0; j<m; j++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(arr[b] - arr[a-1]);
+        }
+    }
+}
