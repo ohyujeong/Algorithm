06c12a2df73ddc2d1406c194f7c946777e8a8416
ohyujeong <ohlife4@gmail.com>
2023-10-26 10:58:01 +0900

    프로그래머스 네트워크(dfs)

---
+++ src/프로그래머스/dfs1.java
@@ -0,0 +1,38 @@
+package 프로그래머스;
+
+import java.util.*;
+
+//네트워크
+public class dfs1 {
+
+
+    static boolean[] visited;
+    static int answer =0;
+
+    public void dfs(int i, int[][] computers, int n){
+
+        visited[i] = true;
+
+        for(int j=0; j<n; j++){
+            //방문하지 않은 컴퓨터이고, 그 컴퓨터가 j번째 컴퓨터랑 연결되어 있으면
+            if(!visited[j] && computers[i][j] ==1){
+                dfs(j, computers, n);
+            }
+        }
+    }
+
+    public int solution(int n, int[][] computers){
+
+        visited = new boolean[n];
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                answer++;
+                dfs(i, computers, n);
+            }
+        }
+
+        return answer;
+    }
+
+}
