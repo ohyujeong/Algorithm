a3a5c760ca5eba6ea633f4e7c65633c6951f9e79
ohyujeong <ohlife4@gmail.com>
2024-01-24 15:15:55 +0900

    네트워크 (dfs)

--- src/프로그래머스2024/pro3.java
+++ src/프로그래머스2024/pro3.java
@@ -8,41 +8,33 @@ import java.util.Queue;
 //네트워크
 public class pro3 {
 
-    public static int answer = 0;
-    public static boolean[] visited;
+    static boolean[] visited;
+    static int answer =0;
 
-    Queue<Integer> q = new LinkedList<>();
+    public void dfs(int i, int[][] computers, int n){
 
-    private void bfs(int index, int[][] computers, int n){
+        visited[i] = true;
 
-       visited[index] = true;
-
-       while(!q.isEmpty()){
-           int tmp = q.poll();
-           for(int i=0; i<n; i++){
-               if(computers[tmp][i] == 1){
-                   q.offer(i);
-                   computers[tmp][i] = 0;
-                   visited[i] = true;
-               }
-           }
-       }
-   }
-
-    public int solution(int n, int[][] computers) {
-
-        visited = new boolean[n];
-
-        //시작점이 정해진게 아니니까 고정호출이 아니라 방문하지 않은 인덱스에 대해 i로 호출
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                answer++;
-                q.offer(i);
-                bfs(i, computers, n);
+        for(int j=0; j<n; j++){
+            if(!visited[j] && computers[i][j] ==1){
+                dfs(j, computers, n);
             }
         }
 
-        System.out.println(answer);
+
+    }
+
+    public int solution(int n, int[][] computers){
+
+        visited = new boolean[n];
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                answer++;
+                dfs(i, computers, n);
+            }
+        }
+
         return answer;
     }
 
