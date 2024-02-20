b8dc7d30bafc792ba74e5edff989669e6c79dd39
ohyujeong <ohlife4@gmail.com>
2024-02-20 15:14:20 +0900

    숫자 문자열과 영단어(replace 함수)

--- src/프로그래머스2024/pro37.java
+++ src/프로그래머스2024/pro37.java
@@ -19,41 +19,44 @@ public class pro37 {
 
         StringBuilder sb = new StringBuilder();
 
-        HashMap<String, Integer> hm = new HashMap<>();
-
         String[] dic = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i=0; i<dic.length; i++){
-            hm.put(dic[i],i);
+
+
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(dic[i], Integer.toString(i));
         }
+//
+//        HashMap<String, Integer> hm = new HashMap<>();
+        //        for(int i=0; i<dic.length; i++){
+//            hm.put(dic[i],i);
+//        }
+//
+//        //3글자, 4글자, 5글자
+//        for(int i=0; i<s.length(); i++){
+//            String tmp = s.split("")[i];
+//            //숫자일 경우 그대로 붙여줌
+//            if(check(tmp)){
+//                sb.append(tmp);
+//            }
+//            else{
+//                for(int j=3; j<6; j++){
+//                    if(i+j<=s.length()){
+//                        String idx = s.substring(i, i+j);
+//                        if(hm.containsKey(idx)){
+//                            sb.append(hm.get(idx));
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
-        //3글자, 4글자, 5글자
-        for(int i=0; i<s.length(); i++){
-            String tmp = s.split("")[i];
-            //숫자일 경우 그대로 붙여줌
-            if(check(tmp)){
-                sb.append(tmp);
-            }
-            else{
-                for(int j=3; j<6; j++){
-                    if(i+j<=s.length()){
-                        String idx = s.substring(i, i+j);
-                        if(hm.containsKey(idx)){
-                            sb.append(hm.get(idx));
-                        }
-                    }
-                }
-            }
-        }
-
-
-        int answer = Integer.parseInt(sb.toString());
-        return answer;
+        return Integer.parseInt(s);
     }
 
 
     public static void main(String[] args){
         pro37 T = new pro37();
 
-        T.solution("123");
+        T.solution("one4seveneight");
     }
 }
