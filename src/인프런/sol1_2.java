abe2874ac10f9b0aadb6d683e8ce8d4eac2efbd6
ohyujeong <ohlife4@gmail.com>
2024-03-08 14:39:35 +0900

    청소(idx 재조정 %로 하기)

--- src/인프런/sol1_2.java
+++ src/인프런/sol1_2.java
@@ -15,7 +15,7 @@ public class sol1_2 {
 
         Queue<int[]> q = new LinkedList<>();
 
-        q.offer(new int[]{0,0});
+//        q.offer(new int[]{0,0});
 
         int cnt =0;
         int[] answer = new int[2];
@@ -23,36 +23,49 @@ public class sol1_2 {
         //방향 인덱스
         int idx = 0;
 
+        int x=0;
+        int y=0;
         while (cnt<k) {
-                int[] tmp = q.poll();
-                //현재 방향으로 이동 해봄
-                for(int i=idx; i<4; i++){
-                    int nx = tmp[0] + dx[i];
-                    int ny = tmp[1] + dy[i];
-                    if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==0){
-                        q.offer(new int[]{nx,ny});
-                    }
-                    if(!q.isEmpty()){
-                        idx=i;
-                        cnt++;
-                        break;
-                    }
-                    //방향 바꿈
-                    else{
-                        cnt++;
-                        //방향 바꾸느라 1초 썼는데 시간 끝이면 현재 위치 반환
-                        if(cnt==k){
-                            return tmp;
-                        }
-                        if(i==3){
-                            i=-1;
-                        }
-                    }
-                }
+            cnt++;
+            int nx = x+dx[idx];
+            int ny = y+dy[idx];
+            if(nx<0 || nx>=n || ny<0 || ny>=n || board[nx][ny]==1){
+                idx = (idx+1)%4;
+                continue;
             }
+            x = nx;
+            y = ny;
+//                int[] tmp = q.poll();
+                //현재 방향으로 이동 해봄
+//                for(int i=idx; i<4; i++){
+//                    int nx = tmp[0] + dx[i];
+//                    int ny = tmp[1] + dy[i];
+//                    if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==0){
+//                        q.offer(new int[]{nx,ny});
+//                    }
+//                    if(!q.isEmpty()){
+//                        idx=i;
+//                        cnt++;
+//                        break;
+//                    }
+//                    //방향 바꿈
+//                    else{
+//                        cnt++;
+//                        //방향 바꾸느라 1초 썼는데 시간 끝이면 현재 위치 반환
+//                        if(cnt==k){
+//                            return tmp;
+//                        }
+//                        if(i==3){
+//                            i=-1;
+//                        }
+//                    }
+//                }
+            }
+//
+//        answer = q.poll();
 
-
-        answer = q.poll();
+        answer[0] = x;
+        answer[1] = y;
         return answer;
     }
 
