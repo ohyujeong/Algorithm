0ec1eae3d9f193eee4091e832f17c8131d23ba90
ohyujeong <ohlife4@gmail.com>
2024-04-07 18:04:43 +0900

    파괴되지 않는 건물(2차원 누적합)

--- src/프로그래머스2024/pro70.java
+++ src/프로그래머스2024/pro70.java
@@ -23,50 +23,37 @@ public class pro70 {
             int c2 = skill[i][4];
 
             int degree = skill[i][5];
-
             if(type==1){
-                damage[r1][c1]-=degree;
-                damage[r2+1][c2+1]-=degree;
-
-                damage[r1][c2+1]+=degree;
-                damage[r2+1][c1]+=degree;
+                degree*=-1;
             }
-            else{
-                damage[r1][c1]+=degree;
-                damage[r2+1][c2+1]+=degree;
+            damage[r1][c1]+=degree;
+            damage[r2+1][c2+1]+=degree;
 
-                damage[r1][c2+1]-=degree;
-                damage[r2+1][c1]-=degree;
-            }
+            damage[r1][c2+1]-=degree;
+            damage[r2+1][c1]-=degree;
         }
 
 
-        // 누적합 계산
-        int[][] sum = new int[row+1][col+1];
-        // 위->아래
-        for(int i=0; i<row+1; i++) {
-            sum[i][0] = damage[i][0];
-            for(int j=1; j<col+1; j++) {
-                sum[i][j] = damage[i][j] + sum[i][j - 1];
-            }
-        }
-        // 왼->오
-        for(int j=0; j<col+1; j++) {
-            for(int i=1; i<row+1; i++){
-                sum[i][j] += sum[i-1][j];
+        for(int c=1; c<col; c++){
+            for(int r=0; r<row; r++){
+                damage[r][c] += damage[r][c-1];
             }
         }
 
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                board[i][j]+=sum[i][j];
+        for(int r=1; r<row; r++){
+            for(int c=0; c<col; c++){
+                damage[r][c] += damage[r-1][c];
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                board[i][j] += damage[i][j];
                 if(board[i][j]>=1){
                     answer++;
                 }
             }
         }
-
-        System.out.println(answer);
         return answer;
     }
 
