9298b738f2cc0bdc83865d848f1ae3a26c89dd71
ohyujeong <ohlife4@gmail.com>
2024-04-14 09:48:19 +0900

    쇠막대기(스택 자료구조)

--- src/자료구조/boj10799.java
+++ src/자료구조/boj10799.java
@@ -6,7 +6,6 @@ import java.util.*;
 //쇠막대기
 public class boj10799 {
 
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
