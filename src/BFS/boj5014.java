a062736d27ae864d759346987d78afacffa24739
ohyujeong <ohlife4@gmail.com>
2024-04-19 10:53:57 +0900

    스타트링크(bfs)

--- src/BFS/boj5014.java
+++ src/BFS/boj5014.java
@@ -8,49 +8,40 @@ public class boj5014 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] button = new int[5];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        //F, S, G, U, D
-        for(int i=0; i<button.length; i++){
-            button[i] = Integer.parseInt(st.nextToken());
-        }
-
         //1, F층을 넘어서 갈 순 없음
         //S -> G
-        //button[3] U버튼->위로 U, button[4] D버튼->아래로
-        int F = button[0];
-        int S = button[1];
-        int G = button[2];
-        int U = button[3];
-        int D = button[4];
+        int F = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
 
+        int[] dir = new int[2];
+        dir[0] = Integer.parseInt(st.nextToken());
+        dir[1] = -(Integer.parseInt(st.nextToken()));
+
+        int[] visited = new int[F+1];
         Queue<Integer> q = new LinkedList<>();
         q.offer(S);
 
-        boolean[] visited = new boolean[F+1];
+        //방문
+        visited[S] = 1;
 
-        int L = 0;
         while(!q.isEmpty()){
-            int len = q.size();
-            for(int i=0; i<len;  i++){
-                int cur = q.poll();
-                if(cur==G){
-                    System.out.println(L);
-                    return;
+            int cur = q.poll();
+            if(cur==G){
+                System.out.println(visited[G]-1);
+                return;
+            }
+            for(int j=0; j<2; j++){
+                int next = cur+dir[j];
+                if(next>F || next<1){
+                    continue;
                 }
-                int up = cur+U;
-                int down = cur-D;
-                if(!visited[up] && up<=F){
-                    visited[up] = true;
-                    q.offer(up);
-                }
-                if(!visited[down] && down>=1){
-                    visited[down] = true;
-                    q.offer(down);
+                if(visited[next]==0){
+                    visited[next] = visited[cur]+1;
+                    q.offer(next);
                 }
             }
-            L++;
         }
 
         System.out.println("use the stairs");
