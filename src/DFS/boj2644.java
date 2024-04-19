ebb8d44b7e35356d550bec1f0214bb1a47eee2f1
ohyujeong <ohlife4@gmail.com>
2024-04-19 14:38:14 +0900

    촌수계산(그래프 자료형 변경)

--- src/DFS/boj2644.java
+++ src/DFS/boj2644.java
@@ -8,7 +8,7 @@ public class boj2644 {
     static int n,m;
     static int target, start;
     static boolean[] visited;
-    static ArrayList<Integer>[] graph;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
     static int ans = -1;
 
@@ -18,8 +18,8 @@ public class boj2644 {
             ans=cnt;
             return;
         }
-        for(int i=0; i<graph[idx].size(); i++){
-            int next = graph[idx].get(i);
+        for(int i=0; i<graph.get(idx).size(); i++){
+            int next = graph.get(idx).get(i);
             if(!visited[next]){
                 dfs(next, cnt+1);
             }
@@ -36,10 +36,9 @@ public class boj2644 {
         m = Integer.parseInt(br.readLine());
 
         visited = new boolean[n+1];
-        graph = new ArrayList[n+1];
 
-        for(int i=1; i<=n; i++){
-            graph[i] = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
         }
 
         int x,y;
@@ -47,12 +46,12 @@ public class boj2644 {
             st = new StringTokenizer(br.readLine());
             x=Integer.parseInt(st.nextToken());
             y=Integer.parseInt(st.nextToken());
-            graph[x].add(y);
-            graph[y].add(x);
+            graph.get(x).add(y);
+            graph.get(y).add(x);
         }
 
         for(int i=1; i<=n; i++){
-            Collections.sort(graph[i]);
+            Collections.sort(graph.get(i));
         }
 
         dfs(start, 0);
