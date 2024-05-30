f1ee07d33e7ad80fe2ea498d27c1a79fa19628e1
ohyujeong <ohlife4@gmail.com>
2024-05-30 13:12:04 +0900

    동물원

--- src/다시풀기/boj1309.java
+++ src/다시풀기/boj1309.java
@@ -1,28 +1,25 @@
 package 다시풀기;
 
-import java.io.*;
+import java.util.*;
 
 public class boj1309 {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
         int[][] dp = new int[N+1][3];
 
         dp[1][0] = dp[1][1] = dp[1][2] = 1;
-
         for(int i=2; i<=N; i++){
-            dp[i][0] = (dp[i-1][1]+dp[i-1][2]+dp[i-1][0]) %9901;
-            dp[i][1] = (dp[i-1][0]+dp[i-1][2]) %9901;
-            dp[i][2] = (dp[i-1][0]+dp[i-1][1]) %9901;
+            dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % 9901;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901 ;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901;
         }
 
-
-        System.out.println((dp[N][0] + dp[N][1] +dp[N][2])%9901);
-
-
+        int answer = (dp[N][0]+dp[N][1]+dp[N][2]) % 9901;
+        System.out.println(answer);
 
     }
 }
