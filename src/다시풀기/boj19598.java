1e98d47f8c31dca81e919236c72966f6288a58ef
ohyujeong <ohlife4@gmail.com>
2024-05-30 12:49:53 +0900

    최소 회의실 개수

--- src/다시풀기/boj19598.java
+++ src/다시풀기/boj19598.java
@@ -1,26 +1,24 @@
 package 다시풀기;
 
-import java.io.*;
 import java.util.*;
+
+class Meet{
+    int start;
+    int end;
+    public Meet(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
 
 public class boj19598 {
 
-    static class Meet{
-        int start;
-        int end;
-        public Meet(int start, int end){
-            this.start = start;
-            this.end = end;
-        }
-    }
+    public static void main(String[] args)  {
 
-    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        //시작 시간이 빠른 순으로 내림차순 정렬
-        PriorityQueue<Meet> q = new PriorityQueue<>(new Comparator<Meet>() {
+        PriorityQueue<Meet> pQ = new PriorityQueue<>(new Comparator<Meet>() {
             @Override
             public int compare(Meet o1, Meet o2) {
                 if(o1.start==o2.start){
@@ -30,33 +28,30 @@ public class boj19598 {
             }
         });
 
-        StringTokenizer st;
-        while(n-->0){
-            st = new StringTokenizer(br.readLine());
-
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            q.offer(new Meet(start,end));
+        for(int i=0; i<N; i++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            pQ.offer(new Meet(start,end));
         }
 
         PriorityQueue<Integer> room = new PriorityQueue<>();
-        room.add(q.poll().end);
 
-        while(!q.isEmpty()){
-            int prev = room.peek();
-            Meet next = q.poll();
-            //다음 회의 시작 시간이 이전회의의 끝나는 시간보다 이후면 같은 회의실에서 할 수 있음
-            if(next.start >= prev){
-                //기존회의는 회의실 나가줌
+        //첫 번째로 시작 하는 회의의 끝나는 시간
+        room.add(pQ.poll().end);
+
+        while(!pQ.isEmpty()){
+            Meet nextMeet = pQ.poll();
+            int nextStart = nextMeet.start;
+
+            //회의실의 가장 앞(가장 빨리 끝나는 회의) 가 nextStart보다 크면 새로운 회의실 열어야 함
+            //회의실의 가장 빨리 끝나는 회의가 nextStart와 시간이 같거나, 작으면 해당 회의실에서 다음 회의 진행
+            if(room.peek()<=nextStart){
                 room.poll();
             }
-            //시작시간이 끝나는 시간보다 이전이면 기존회의는 회의실 안 나가고
-            //다음 회의는 다른 회의실에서 회의 시작
-            room.add(next.end);
+            room.add(nextMeet.end);
         }
 
-        int answer = 0;
-        answer = room.size();
-        System.out.println(answer);
+        System.out.println(room.size());
+
     }
 }
